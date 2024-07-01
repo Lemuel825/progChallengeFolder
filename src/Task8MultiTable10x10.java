b80ad07cc586f844d3c3b5b3d080a68982d01c43
Lemuel825 <140239366+Lemuel825@users.noreply.github.com>
2024-07-01 16:58:13 +0800

    Updated

--- src/Task8MultiTable10x10.java
+++ src/Task8MultiTable10x10.java
@@ -1,8 +1,7 @@
 public class Task8MultiTable10x10 {
     public static void main(String[] args) {
-        int size = 10; // Size of the multiplication table
+        int size = 10; 
 
-        // Print the table
         for (int i = 1; i <= size; i++) {
             for (int j = 1; j <= size; j++) {
                 System.out.printf("%4d", i * j);
